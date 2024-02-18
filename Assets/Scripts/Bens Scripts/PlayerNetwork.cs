70eacd2c62c0781a914a8e064480dccc1f98d7c2
Ben <ben.a.osborn@gmail.com>
2024-02-18 23:11:01 +0000

    New Network

---
+++ Assets/Scripts/Bens Scripts/PlayerNetwork.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+public class PlayerNetwork : NetworkBehaviour {
+    [SerializeField] private float speed;
+    void FixedUpdate() {
+        if (IsLocalPlayer) {
+            float movement = Input.GetAxis("Horizontal");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0.0f);
+        }
+    }
+}
+
