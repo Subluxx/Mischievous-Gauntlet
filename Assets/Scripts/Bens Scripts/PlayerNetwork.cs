49b52d49cf89b505b7be64a7215f22d88c99a733
Ben <ben.a.osborn@gmail.com>
2024-02-20 20:09:15 +0000

    More networking

--- Assets/Scripts/Bens Scripts/PlayerNetwork.cs
+++ Assets/Scripts/Bens Scripts/PlayerNetwork.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using Unity.Netcode;
 public class PlayerNetwork : NetworkBehaviour {
     [SerializeField] private float speed;
+    [SerializeField] private Transform platformObjectPrefab;
     void FixedUpdate() {
         if (IsLocalPlayer) {
             float movement = Input.GetAxis("Horizontal");
             GetComponent<Rigidbody2D>().velocity = new Vector2(movement * speed, 0.0f);
         }
+        Transform platformObjectTransform = Instantiate(platformObjectPrefab);
+        platformObjectPrefab.GetComponent<NetworkObject>().Spawn(true);
     }
 }
 
