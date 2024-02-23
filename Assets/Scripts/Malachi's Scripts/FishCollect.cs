f8e6ea59be140f006bacf69accb474865e6334aa
MalachiLudford-Powell <146730097+MalachiLudford-Powell@users.noreply.github.com>
2024-02-23 13:59:47 +0000

    barrier for player

--- Assets/Scripts/Malachi's Scripts/FishCollect.cs
+++ Assets/Scripts/Malachi's Scripts/FishCollect.cs
@@ -7,21 +7,23 @@ public class FishCollect : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag ("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             Destroy(gameObject);
-           
-        }
+
+       }
+
     }
 }
+
