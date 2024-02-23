ebff7bd4752db3d69f6842f256c1622f51b735c2
Joe Hill <91340791+Subluxx@users.noreply.github.com>
2024-02-23 13:59:02 +0000

    seagulrandomspawn

--- Assets/Scripts/MiniGame1/SeaGullSpawnBehaviour.cs
+++ Assets/Scripts/MiniGame1/SeaGullSpawnBehaviour.cs
@@ -11,12 +11,21 @@ public class SeaGullSpawnBehaviour : MonoBehaviour
     [SerializeField] public float fireForce = 20f;
     [SerializeField] public float spawnDelay = 5.0f;
     [SerializeField] public int spawnDir = 0;
+    public float rotationSpawn;
     public int ParrotSpawnChance;
     [SerializeField] public int MaxRand = 15;
     // Start is called before the first frame update
 
     private void Awake()
     {
+        if (spawnDir == -1)
+        {
+            rotationSpawn = 0f;
+        }
+        else if (spawnDir == 1)
+        {
+            rotationSpawn = 90f;
+        }
         StartCoroutine(SpawnSeaGull(spawnDelay));
     }
 
@@ -24,26 +33,15 @@ public class SeaGullSpawnBehaviour : MonoBehaviour
     {
         yield return new WaitForSeconds(delayVar);
         ParrotSpawnChance = Random.Range(0, MaxRand);
-        if (ParrotSpawnChance == MaxRand)
+        if (ParrotSpawnChance == MaxRand - 1)
         {
-
+            GameObject parrot = Instantiate(ParrotPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, rotationSpawn));
+            parrot.GetComponent<Rigidbody2D>().AddForce(transform.up * fireForce, ForceMode2D.Impulse);
         }
         else
         {
-
-        }
-        
-        if(spawnDir == -1)
-        {
-            GameObject SeaGull = Instantiate(SeaGullPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, 0f));
+            GameObject SeaGull = Instantiate(SeaGullPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, rotationSpawn));
             SeaGull.GetComponent<Rigidbody2D>().AddForce(transform.up * fireForce, ForceMode2D.Impulse);
-            
-        }
-        else if(spawnDir == 1)
-        {
-            GameObject SeaGull = Instantiate(SeaGullPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, 90f));
-            SeaGull.GetComponent<Rigidbody2D>().AddForce(transform.right * fireForce, ForceMode2D.Impulse);
-            
         }
         StartCoroutine(SpawnSeaGull(spawnDelay));
     }
