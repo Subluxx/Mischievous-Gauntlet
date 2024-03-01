3046bf6cb676000f158569db63c19d2fe8e9be83
Joe Hill <91340791+Subluxx@users.noreply.github.com>
2024-03-01 14:10:02 +0000

    camerashake

--- Assets/Scripts/MiniGame1/CannonScript.cs
+++ Assets/Scripts/MiniGame1/CannonScript.cs
@@ -7,11 +7,16 @@ public class CannonScript : MonoBehaviour
     public GameObject ProjectilePrefab;
     public Transform firePoint;
     public float fireForce = 20f;
+    public ShakeBehaviour shakeBehaviour;
+    [SerializeField] public float duration = 1f;
+    [SerializeField] public float magnitude = 1f;
     // Start is called before the first frame update
 
     public void Fire()
     {
         GameObject projectile = Instantiate(ProjectilePrefab, firePoint.position, firePoint.rotation * Quaternion.Euler(0, 0, 180f));
         projectile.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+        
+        StartCoroutine(shakeBehaviour.Shake(duration, magnitude));
     }
 }
