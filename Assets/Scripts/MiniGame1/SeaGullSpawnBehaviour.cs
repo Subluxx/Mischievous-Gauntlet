90638952d6aa1aec6601c5a34f29906d66dad3c9
Joe Hill <91340791+Subluxx@users.noreply.github.com>
2024-03-06 18:01:07 +0000

    postprocessing

--- Assets/Scripts/MiniGame1/SeaGullSpawnBehaviour.cs
+++ Assets/Scripts/MiniGame1/SeaGullSpawnBehaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using Unity.VisualScripting;
+using UnityEditor.Timeline;
 using UnityEngine;
 
 public class SeaGullSpawnBehaviour : MonoBehaviour
@@ -10,39 +11,52 @@ public class SeaGullSpawnBehaviour : MonoBehaviour
     public GameObject ParrotPrefab;
     [SerializeField] public float fireForce = 20f;
     [SerializeField] public float spawnDelay = 5.0f;
-    [SerializeField] public int spawnDir = 0;
-    public float rotationSpawn;
+    [SerializeField] public bool FlyingRight;
     public int ParrotSpawnChance;
-    [SerializeField] public int MaxRand = 15;
+    [SerializeField] public int maxRand;
+    Vector3 spawnPosition;
+    float yMoving;
+    bool ymovingup = false;
+    [SerializeField] float boundingArea;
     // Start is called before the first frame update
 
     private void Awake()
     {
-        if (spawnDir == -1)
-        {
-            rotationSpawn = 0f;
-        }
-        else if (spawnDir == 1)
-        {
-            rotationSpawn = 90f;
-        }
+        spawnPosition = transform.position;
+        yMoving = transform.position.y;
         StartCoroutine(SpawnSeaGull(spawnDelay));
     }
-
     IEnumerator SpawnSeaGull(float delayVar)
     {
         yield return new WaitForSeconds(delayVar);
-        ParrotSpawnChance = Random.Range(0, MaxRand);
-        if (ParrotSpawnChance == MaxRand - 1)
+        ParrotSpawnChance = Random.Range(0, maxRand);
+        if (ParrotSpawnChance == maxRand / 2)
         {
-            GameObject parrot = Instantiate(ParrotPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, rotationSpawn));
+            GameObject parrot = Instantiate(ParrotPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, 0));
             parrot.GetComponent<Rigidbody2D>().AddForce(transform.up * fireForce, ForceMode2D.Impulse);
         }
         else
         {
-            GameObject SeaGull = Instantiate(SeaGullPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, rotationSpawn));
+            GameObject SeaGull = Instantiate(SeaGullPrefab, transform.position, transform.rotation * Quaternion.Euler(0, 0, 90));
             SeaGull.GetComponent<Rigidbody2D>().AddForce(transform.up * fireForce, ForceMode2D.Impulse);
         }
+        if (yMoving > spawnPosition.y + boundingArea)
+        {
+            ymovingup = true;
+        }
+        else if (yMoving < spawnPosition.y - boundingArea + boundingArea)
+        {
+            ymovingup = false;
+        }
+        if (!ymovingup)
+        {
+            yMoving += 1f;
+        }
+        else
+        {
+            yMoving -= 1f;
+        }
+        transform.localPosition = new Vector3(spawnPosition.x, yMoving, spawnPosition.z);
         StartCoroutine(SpawnSeaGull(spawnDelay));
     }
 }
