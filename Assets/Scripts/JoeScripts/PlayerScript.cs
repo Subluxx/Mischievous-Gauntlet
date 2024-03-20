c863540cb2ee9c85dccf90a07dddc313b33d86ee
Joe Hill <josephphill@me.com>
2024-03-20 18:54:51 +0000

    a

--- Assets/Scripts/JoeScripts/PlayerScript.cs
+++ Assets/Scripts/JoeScripts/PlayerScript.cs
@@ -23,7 +23,7 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (_isCameraNotNull) mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition = _camera.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetButtonDown("Fire1"))
         {
             cannon.Fire();
